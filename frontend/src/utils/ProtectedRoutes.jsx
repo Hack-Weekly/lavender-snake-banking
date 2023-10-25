0a49d2f9f7fc20f7f94e0402de2893e8b53871ae
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-10-26 00:52:36 +0530

    use space indent

--- frontend/src/utils/ProtectedRoutes.jsx
+++ frontend/src/utils/ProtectedRoutes.jsx
@@ -3,9 +3,9 @@ import { Outlet, Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 const ProtectedRoutes = () => {
-	const { user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
-	return user ? <Outlet /> : <Navigate to="/signin" />;
+  return user ? <Outlet /> : <Navigate to="/signin" />;
 };
 
 export default ProtectedRoutes;
