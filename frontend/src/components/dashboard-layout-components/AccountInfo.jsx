32484b9053949ece69d53d704616e06427ae3fc3
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-10-31 14:17:18 +0530

    dashboard layout

--- frontend/src/components/dashboard-layout-components/AccountInfo.jsx
+++ frontend/src/components/dashboard-layout-components/AccountInfo.jsx
@@ -1,16 +1,12 @@
-
 const AccountInfo = ({ accInfo }) => {
-    return (
-        <div className={"w-72 h-52 flex flex-col rounded-xl bg-[#3D7AE5]/10 text-white"}>
-            <div className={"flex py-6 justify-center font-bold text-2xl"}>
-                Account Number
-            </div>
-            <div className={"flex py-1 justify-center text-2xl"}>
-                {accInfo}
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="w-72 h-52 p-4 flex flex-col rounded-xl bg-[#3D7AE5]/10 text-white">
+      <div className="flex justify-center font-bold text-xl mb-4">
+        Account Number
+      </div>
+      <div className="flex py-1 justify-center text-lg">{accInfo}</div>
+    </div>
+  );
 };
 
 export default AccountInfo;
--- frontend/src/components/dashboard-layout-components/NotificationPanel.jsx
+++ frontend/src/components/dashboard-layout-components/NotificationPanel.jsx
@@ -1,36 +1,30 @@
-import {LayoutDashboardIcon} from "lucide-react";
-
-const NotificationPanel = ( { notifications } ) => {
-    return (
-        <div className={"w-72 min-h-[55vh] max-h-[calc(100vh-3rem)] flex flex-col rounded-xl bg-[#3D7AE5]/10 text-white"}>
-            <div className={"flex py-6 justify-center font-bold text-2xl"}>
-                Notifications
-            </div>
-            <div>
-                <ul>
-                    {notifications.map((notifLine) => {
-                        return (
-                            <li key={notifLine.heading}>
-                                <div>
-                                    <span>
-                                        {notifLine.type}
-                                    </span>
-                                    <span>
-                                        {notifLine.heading}
-                                    </span>
-                                </div>
-                                <div>
-                                    {notifLine.information}
-                                </div>
-
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-
-        </div>
-    );
+const NotificationPanel = ({ notifications }) => {
+  return (
+    <div
+      className={
+        "w-72 min-h-[55vh] max-h-[calc(100vh-3rem)] p-4 flex flex-col rounded-xl bg-[#3D7AE5]/10 text-white"
+      }
+    >
+      <div className="flex justify-center font-bold text-xl mb-4">
+        Notifications
+      </div>
+      <div className="text-sm">
+        <ul>
+          {notifications.map((notifLine) => {
+            return (
+              <li key={notifLine.heading}>
+                <div>
+                  <span>{notifLine.type}</span>
+                  <span>{notifLine.heading}</span>
+                </div>
+                <div>{notifLine.information}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default NotificationPanel;
