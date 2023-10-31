ece2989b2fa766d86044b7a8440b94e6d2e46801
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-10-31 15:41:36 +0530

    signup page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         darkest: "#160524",
         dark: "#4A00C5",
         accent: "#3E79E5",
-        grey: "#5B5B5B",
+        gray: "#5B5B5B",
         light: "#D3E1F5",
         lightest: "#FFFFFF",
       },
