6d16c2951efb41a118b81e894fbbe8123f03e547
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-10-31 15:51:01 +0530

    signin page

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -8,17 +8,37 @@ const Signin = () => {
 
   const signIn = () => {
     setUser("123");
-    navigate("/dashboard");
+    navigate("/");
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <h1>Sign In</h1>
-      <button onClick={signIn}>Sign In</button>
-      <p>
-        Haven&apos;t signed up yet? <Link to="/signup">Sign Up now</Link>
+    <div className="flex flex-col items-center gap-6">
+      <h1 className="text-5xl font-bold">Welcome Back!</h1>
+      <p className="text-grey text-lg text-center">Sign in to continue</p>
+      <form className="flex flex-col gap-3 w-80">
+        <div className="flex flex-col gap-0.5">
+          <label htmlFor="email" className="">
+            Email
+          </label>
+          <input type="email" className="input" id="email" />
+        </div>
+        <div className="flex flex-col gap-0.5">
+          <label htmlFor="password" className="">
+            Password
+          </label>
+          <input type="password" className="input" id="password" />
+        </div>
+        <button className="btn mt-5" onClick={signIn}>
+          Sign In
+        </button>
+      </form>
+      <p className="font-medium text-gray mt-2">
+        Haven&apos;t signed up yet?
+        <Link to="/signup" className="link">
+          {" "}
+          Sign Up now
+        </Link>
       </p>
-      <Link to="/dashboard">Go to dashboard</Link>
     </div>
   );
 };
